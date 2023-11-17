890318bf831f2b35914ff5469bc66bbfcabad442
Maciej Kowalski <maciej.ko1444@gmail.com>
2023-11-17 17:06:36 +0100

    output is now right-aligned

--- Table of Squares and Cubes.py
+++ Table of Squares and Cubes.py
@@ -4,4 +4,4 @@ cube = 3
 print("number\tsquare\troot")
 for x in range(0,6):
     number = x
-    print(f"{number}\t{number**square}\t{number**cube}" )
+    print(f"{number:>6}\t{number**square:>6}\t{number**cube:>4}")
